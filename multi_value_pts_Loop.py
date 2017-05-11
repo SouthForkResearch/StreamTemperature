e33e9ffcd3027a31c9ddbfb40849c1c26aab4814
Kris McNyset <kristina.mcnyset@noaa.gov>
2017-05-11 15:10:04 -0700

    used for Minam

--- multi_value_pts_Loop.py
+++ multi_value_pts_Loop.py
@@ -17,9 +17,9 @@ gp = arcgisscripting.create()
 # Load required toolboxes...
 gp.AddToolbox("C:\\Program Files (x86)\\ArcGIS\\Desktop10.2\\ArcToolbox\\Toolboxes\\Spatial Analyst Tools.tbx")
 
-gp.workspace = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\LST\\LST_s12_2013\\"
-outfolder = "D:\\OneDrive\\work\research\\CHaMP\\\GIS\\LST\\LST_s12_2013\\"
-inPointFeatures = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\coverages\\Middle_Salmon_Panther\\Panth_13_LST.shp"
+gp.workspace = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\LST\\LST_s1_2015\\"
+outfolder = "D:\\OneDrive\\work\research\\CHaMP\\\GIS\\LST\\LST_s1_2015\\"
+inPointFeatures = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\coverages\\Minam\\Minam_15_LST.shp"
 
 readFieldList = open("D:\\OneDrive\\work\\GIS\\8_day_1km_LST\\LST_s1_2001\\gridlist.txt", 'r')
 FieldList = readFieldList.readlines()
@@ -28,7 +28,7 @@ readFieldList.close
 for item in FieldList:
     item = item.replace("\n","")
     print item
-    inraster = "lst_s1213" + item 
+    inraster = "lsts115" + item 
     arcpy.gp.ExtractMultiValuesToPoints_sa(inPointFeatures, [[inraster, item]], "NONE")  
     
     
